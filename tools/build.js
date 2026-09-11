8765a6e7a86c93667b4e4a0c5cd746674995b8f0
Ariel Wexler <arielwex@gmail.com>
2026-09-11 14:01:35 -0400

    WIP html shrinkage

--- tools/build.js
+++ tools/build.js
@@ -40,7 +40,7 @@ async function packageBuild() {
 
   fs.writeFileSync(bundlePath, code);
   fs.writeFileSync(htmlPath,
-    '<!doctype html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1">' +
+    '<html><head>' +
     '<title>PEGACORN BLOOD</title><style>html,body,canvas{width:100%;height:100%;margin:0;background:#224;display:block;overflow:hidden}</style>' +
     '</head><body><canvas></canvas><script>' + code + '</script></body></html>');
 
