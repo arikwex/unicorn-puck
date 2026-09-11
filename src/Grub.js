628cfa9b9455539f31cd8d2cab8d66602a693601
Ariel Wexler <arielwex@gmail.com>
2026-09-11 12:56:40 -0400

    wide attack range

--- src/Grub.js
+++ src/Grub.js
@@ -73,7 +73,7 @@ const AIM_DURATION = 2;
 // reverse via aimProgress counting back down to 0.
 const RECOVER_DURATION = 0.35;
 const ATTACK_DELAY_MIN = 1;
-const ATTACK_DELAY_MAX = 2;
+const ATTACK_DELAY_MAX = 4;
 const ATTACK_RANGE = 600;
 const PROJECTILE_SPEED = 340;
 const HEALTH_BAR_SHOW_DURATION = 2;
